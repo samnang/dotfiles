23d2eff70fda21867a5df0ee0f5d17d855a8891b
Samnang Chhun <samnang.chhun@gmail.com>
2012-03-19 09:33:17 +0700

    Added more links to dotfiles

--- installer.sh
+++ installer.sh
@@ -6,4 +6,12 @@ ln -s ~/.dotfiles/vim ~/.vim
 ln -s ~/.dotfiles/vimrc ~/.vimrc
 ln -s ~/.dotfiles/gvimrc ~/.gvimrc
 
+ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/bash_aliases ~/.bash_aliases
+ln -s ~/.dotfiles/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/irbrc ~/.irbrc
+ln -s ~/.dotfiles/gemrc ~/.gemrc
+ln -s ~/.dotfiles/gemrc ~/.gemrc
+ln -s ~/.dotfiles/gitconfig ~/.gitconfig
+
 git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
