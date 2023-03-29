43c0489e3c2562a3119b7b4798b74d10df4de1cd
Samnang Chhun <samnang.chhun@gmail.com>
2023-03-29 12:33:27 +0700

    update dotfiles for dev contaianers

--- install.sh
+++ install.sh
@@ -4,15 +4,18 @@
 # dotfiles for Dev Containers #
 ###############################
 
-brew install starship diff-so-fancy bat
+brew install starship diff-so-fancy bat brew gh
 
-ln -s gemrc ~/.gemrc
-ln -s irbrc ~/.irbrc
-ln -s gitconfig ~/.gitconfig
-ln -s gitignore_global ~/.gitignore_global
-ln -s ackrc ~/.ackrc
-ln -s agignore ~/.agignore
-ln -s gitmessage ~/.gitmessage
-ln -s starship.toml ~/.starship.toml
+dotfiles="$(cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
 
+echo $dotfiles
+
+ln -s "${dotfiles}/gemrc" ~/.gemrc
+ln -s "${dotfiles}/irbrc" ~/.irbrc
+ln -s "${dotfiles}/gitconfig" ~/.gitconfig
+ln -s "${dotfiles}/gitignore_global" ~/.gitignore_global
+ln -s "${dotfiles}/gitmessage" ~/.gitmessage
+ln -s "${dotfiles}/starship.toml" ~/.starship.toml
+
+echo "alias cat='bat'" >> ~/.zshrc
 echo 'eval "$(starship init zsh)"' >> ~/.zshrc
