64835493114aba57e04f3bc51de84707323c4c71
Samnang Chhun <samnang.chhun@gmail.com>
2023-04-20 15:25:19 +0700

    Configure keymap toggleterm

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,9 +1,20 @@
 return {
   {
     "akinsho/toggleterm.nvim",
-    config = true,
     keys = {
-      { "<leader>$", "<Cmd>ToggleTerm()<CR>", desc = "Toggle Term" },
+      { "<leader>$", "<Cmd>ToggleTerm direction=float<CR>", desc = "Toggle Term" },
     },
+    config = function()
+      require("toggleterm").setup()
+
+      -- https://github.com/akinsho/toggleterm.nvim#terminal-window-mappings
+      function _G.set_terminal_keymaps()
+        local opts = { buffer = 0 }
+        vim.keymap.set("t", "<esc>", [[<Cmd>ToggleTerm<CR>]], opts)
+        vim.keymap.set("t", "jk", [[<C-\><C-n>]], opts)
+      end
+
+      vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
+    end,
   },
 }
