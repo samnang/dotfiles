7503e8ba09d5fce7043251a211a50c092119bd98
Samnang Chhun <samnang.chhun@gmail.com>
2023-06-08 15:04:21 +0700

    Update to include Rails snippet

--- nvim/lua/plugins/coding.lua
+++ nvim/lua/plugins/coding.lua
@@ -10,4 +10,13 @@ return {
     "kylechui/nvim-surround",
     config = true,
   },
+
+  {
+    "L3MON4D3/LuaSnip",
+    dependencies = { "rafamadriz/friendly-snippets" },
+    config = function()
+      require("luasnip.loaders.from_vscode").load()
+      require("luasnip").filetype_extend("ruby", { "rails" })
+    end,
+  },
 }
