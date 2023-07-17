2bf6401c588309ffe04b40f00e9422d7c7d2f95b
Samnang Chhun <samnang.chhun@gmail.com>
2023-07-17 14:55:18 +0700

    flash.nvim became the default in LazyVim

--- nvim/lua/config/lazy.lua
+++ nvim/lua/config/lazy.lua
@@ -19,7 +19,6 @@ require("lazy").setup({
     { import = "lazyvim.plugins.extras.test.core" },
     { import = "lazyvim.plugins.extras.linting.eslint" },
     { import = "lazyvim.plugins.extras.formatting.prettier" },
-    { import = "lazyvim.plugins.extras.editor.flash" },
 
     -- import/override with your plugins
     { import = "plugins" },
