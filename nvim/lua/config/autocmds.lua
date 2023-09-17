24845d2f38f688d2df22e37f8067f6f06de46544
Samnang Chhun <samnang.chhun@gmail.com>
2023-09-17 14:42:25 +0700

    Update nvim's highlight yanked text

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -12,3 +12,12 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     vim.b.autoformat = false
   end,
 })
+
+-- Highlight on yank (override to have faster timeout which is default: 150)
+-- https://github.com/LazyVim/LazyVim/blob/a72a84972d85e5bbc6b9d60a0983b37efef21b8a/lua/lazyvim/config/autocmds.lua#L13-L19
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("lazyvim_highlight_yank", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({ timeout = 50 })
+  end,
+})
