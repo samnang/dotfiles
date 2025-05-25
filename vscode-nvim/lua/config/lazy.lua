2392ac7622097dc91f427a4f6b019b8f6cc7a361
Samnang Chhun <samnang.chhun@gmail.com>
2025-05-26 00:09:54 +0700

    Update dotfiles

--- vscode-nvim/lua/config/lazy.lua
+++ vscode-nvim/lua/config/lazy.lua
@@ -36,14 +36,18 @@ require("lazy").setup({
     enabled = true, -- check for plugin updates periodically
     notify = false, -- notify on update
   }, -- automatically check for plugin updates
+  clean = {
+    enabled = true, -- enable auto-cleaning of unused plugins
+    notify = true, -- notify when plugins are removed
+  },
   performance = {
     rtp = {
       -- disable some rtp plugins
       disabled_plugins = {
         "gzip",
-        -- "matchit",
-        -- "matchparen",
-        -- "netrwPlugin",
+        "matchit",
+        "matchparen",
+        "netrwPlugin",
         "tarPlugin",
         "tohtml",
         "tutor",
